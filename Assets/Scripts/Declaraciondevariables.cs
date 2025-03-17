201654147c813f75bb006df991b627eb78c217c2
nachuslotus <ignaciodavidoliver@gmail.com>
2025-03-17 08:42:11 -0300

    Initial commit

---
+++ Assets/Scripts/Declaraciondevariables.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Declaraciondevariables : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int tubos_pringles = 40;
+        string marca = "Pringles";
+        bool es_lays = false;
+    
+        Debug.Log(tubos_pringles, marca, es_lays);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
