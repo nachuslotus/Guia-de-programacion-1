5826dd959b6a3a4e30a10b3627768461c8e37de0
nachuslotus <166507478+nachuslotus@users.noreply.github.com>
2025-03-31 08:15:55 -0300

    Ejercicios 3 del 2-2 hecho
    Voy comenzando los ejercicios 4 y 5 de la 2 guía de programación.

--- Assets/Scripts/EnConsolaSinIngreso.cs
+++ Assets/Scripts/EnConsolaSinIngreso.cs
@@ -5,18 +5,29 @@ using UnityEngine;
 public class EnConsolaSinIngreso : MonoBehaviour
 {
 
-    public int Num1;
-    public int Num2;
+    public int num1;
+    public int num2;
+    public int num3;
+    public int num4;
     public int resultado;
     public int resultado2;
 
     // Start is called before the first frame update
     void Start()
     {
-        Num2 = 2;
-        resultado = Num1 + Num2;
-        resultado2 = Num2*Num2 ;
+        num2 = 2;
+        resultado = num1 + num2;
+        resultado2 = num2*num2 ;
         Debug.Log("el resultado de la primera operacion, es 2. Y la multiplicacion de si mismo es " + resultado2 );
+
+        num3 =4;
+        num4 =5;
+
+        Debug.Log(num3+num4);
+        Debug.Log(num3 * num4);
+
+
+
 
     }
 
--- Assets/Scripts/NewBehaviourScript.cs
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -2,15 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnConsolaPorIngesoConInspector : MonoBehaviour
+public class NewBehaviourScript : MonoBehaviour
 {
-
-    public string saludo;
-
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log(" Hola " + saludo);
+        
     }
 
     // Update is called once per frame
